aa717686c011d35369a8e96187143e72dd9f5283
Corvax <vlvete1990@yandex.ru>
2020-05-28 15:26:51 +0300

    setImmediate

--- eventloop.js
+++ eventloop.js
@@ -1,7 +1,15 @@
-console.log('before');
+// console.log('before');
 
-process.nextTick(() => {
-    console.log('tick!');
+// process.nextTick(() => {
+//     console.log('tick!');
+// });
+
+// console.log('after');
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0)
+
+setImmediate(() => {
+    console.log('setImmediate');
 });
 
-console.log('after');
