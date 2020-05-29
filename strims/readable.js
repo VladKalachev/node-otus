c8d1ad76c45e31c368086e9336eb5a39cd190875
Corvax <vlvete1990@yandex.ru>
2020-05-29 14:00:05 +0300

    adding readable and writable strims

---
+++ strims/readable.js
@@ -0,0 +1,12 @@
+const stream = require('stream') 
+
+const readable = (function() {
+    const data = []
+    const $ = new stream.Readable({ objectMode: true, read() {} }) 
+    $.push({ a: 1 })
+    return $
+ })()
+    
+readable.on('data', (data) => { 
+    console.log(data)
+})
