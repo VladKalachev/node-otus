ee2eb9a77d98897fb96eaf6f0a09e43cb82ef06a
Corvax <vlvete1990@yandex.ru>
2020-05-31 01:16:20 +0300

    adding person-service two

--- jest/person-service.test.js
+++ jest/person-service.test.js
@@ -1,24 +1,26 @@
 const PersoneService = require('./person-service');
 
+let service = null;
+
+beforeEach(() => {
+    service = new PersoneService();
+})
+
 test('PersoneService should be created with empty array', () => {
-    const result = new PersoneService();
-    expect(result.getAll()).toStrictEqual([]);
+    expect(service.getAll()).toStrictEqual([]);
 })
 
 test('PersoneService should be save person', () => {
-    const result = new PersoneService();
-    result.save({name: "Name"});
-    expect(result.getAll().length).toBe(1);
+    service.save({name: "Name"});
+    expect(service.getAll().length).toBe(1);
 })
 
 test('PersoneService should be save person', () => {
-    const result = new PersoneService();
-    result.save({name: "Name"});
-    expect(result.getAll().length).toBe(1);
+    service.save({name: "Name"});
+    expect(service.getAll().length).toBe(1);
 })
 
 test('PersoneService should be getByName', () => {
-    const result = new PersoneService();
-    result.save({name: "Name"});
-    expect(result.getByName('Name')).not.toBeNull();
+    service.save({name: "Name"});
+    expect(service.getByName('Name')).not.toBeNull();
 })
