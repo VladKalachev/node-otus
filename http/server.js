a8c011af38fc7e30006a6800a23cf2d4420e1474
Corvax <vlvete1990@yandex.ru>
2020-05-31 13:38:52 +0300

    adding server http

--- http/server.js
+++ http/server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html',
         'Expires': '0'
-    })
+    });
     fs.createReadStream('./index.html').pipe(res);
 })
 
