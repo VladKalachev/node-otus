286e5b0d1228812588238fd77e0d05591f4d85c2
Corvax <vlvete1990@yandex.ru>
2020-05-31 19:06:40 +0300

    adding person get method

--- express-server/server.js
+++ express-server/server.js
@@ -16,4 +16,20 @@ app.get('/hello/:id', (req, res) => {
     res.send(`Hello ${req.params.id}!`);
 });
 
+// GET /person
+app.get('/person', (req, res) => {
+    const person = { name: 'Name'};
+    res.send(person)
+});
+
+app.get('/person/file', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+
+app.get('/person/status', (req, res) => {
+    // 204 - no content
+    res.sendStatus(204);
+});
+
 app.listen(port, () => console.log(`Example app listening on port port!`))
