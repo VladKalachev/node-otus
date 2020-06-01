7ff9f6967c00fdf07914573538df2cca7169662f
Corvax <vlvete1990@yandex.ru>
2020-06-01 10:31:39 +0300

    adding body-parser

--- middlewares/server.js
+++ middlewares/server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
 
 // middlewares
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
     console.log(req.ip);
     console.log(req.header('User-Agent'));
@@ -30,11 +33,16 @@ app.get('/person', (req, res) => {
     res.send({ name: "Person" });
 });
 
+// POST / person
+app.post('/person', (req, res) => {
+    console.log('Got body:', req.body);
+    res.send('Hello')
+});
+
 // GET /user
 app.get('/user', (req, res) => {
     console.log('get user')
     res.send({ name: "User" });
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
